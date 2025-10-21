22e56352d26ba07aee5427371126099510a215bd
oliviabaudet <olivia.baudet@gmail.com>
2025-10-21 18:01:04 +0200

    Structures cellule, liste et liste_adjacence dans .h

--- TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.c
+++ TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.c
@@ -5,21 +5,6 @@
 #include "utils.h"
 
 
-typedef struct cell {
-    int destination;
-    float probability;
-    struct cell *next;
-} Cell;
-
-typedef struct {
-    Cell *head;
-} list;
-
-typedef struct {
-    int size;
-    List *array;
-} adjacencylist;
-
 static char *getID(int i)
 {
     // translate from 1,2,3, .. ,500+ to A,B,C,..,Z,AA,AB,...
--- TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.h
+++ TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.h
@@ -1,5 +1,21 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct cell {
+    int destination;
+    float probability;
+    struct cell *next;
+} Cell;
+
+typedef struct {
+    Cell *head;
+} list;
+
+typedef struct {
+    int size;
+    List *array;
+} adjacencylist;
 
 #endif
