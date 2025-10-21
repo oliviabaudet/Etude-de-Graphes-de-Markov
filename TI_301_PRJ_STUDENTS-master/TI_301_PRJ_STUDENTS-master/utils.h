e8b7863a9772719dfd9ff8ae39f4769198f988b7
oliviabaudet <olivia.baudet@gmail.com>
2025-10-21 17:50:01 +0200

    Structures cellule, liste et liste_adjacence

--- TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.h
+++ TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.h
@@ -1,21 +1,5 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
-#include <stdio.h>
-#include <stdlib.h>
 
-typedef struct cellule {
-    int arrivee;
-    float proba;
-    struct cellule *suiv;
-} cellule;
-
-typedef struct {
-    cellule *head;
-} liste;
-
-typedef struct {
-    int taille;
-    liste *tab;
-} liste_adjacence;
 
 #endif
