fc1cfa729169cb13560cc2542dd2854cb44c1ddb
oliviabaudet <olivia.baudet@gmail.com>
2025-11-22 16:04:05 +0100

    Doxygen (commentaires) sur les structures de utils.h

--- TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.h
+++ TI_301_PRJ_STUDENTS-master/TI_301_PRJ_STUDENTS-master/utils.h
@@ -7,33 +7,74 @@
 
 // ==== Structures ====
 
+/**
+ * @struct cell
+ * @brief Structure représentant une cellule dans une liste chaînée pour le graphe
+ * 
+ * Cette structure est utilisée pour représenter un arc dans le graphe, contenant
+ * la destination et la probabilité associée à cet arc.
+ * 
+ * @var cell::destination
+ * L'identifiant du sommet de destination (entier)
+ * @var cell::probability
+ * La probabilité de transition vers le sommet destination (nombre flottant entre 0 et 1)
+ * @var cell::next
+ * Pointeur vers la prochaine cellule dans la liste chaînée
+ */
 typedef struct cell {
     int destination;
     float probability;
     struct cell *next;
 } Cell;
 
+/**
+ * @struct List
+ * @brief Structure représentant une liste chaînée de cellules
+ * 
+ * Cette structure est utilisée pour stocker une liste d'arcs sortants d'un sommet.
+ * 
+ * @var List::head
+ * Pointeur vers la première cellule de la liste. NULL si la liste est vide.
+ */
 typedef struct {
     Cell *head;
 } List;
 
+/**
+ * @struct AdjacencyList
+ * @brief Structure représentant un graphe sous forme de liste d'adjacence
+ * 
+ * Cette structure permet de représenter un graphe orienté pondéré où chaque sommet
+ * possède une liste de ses voisins sortants.
+ * 
+ * @var AdjacencyList::size
+ * Nombre de sommets dans le graphe
+ * @var AdjacencyList::array
+ * Tableau de listes d'adjacence, une pour chaque sommet
+ */
 typedef struct {
     int size;
     List *array;
 } AdjacencyList;
 
+/**
+ * @struct t_tarjan_vertex
+ * @brief Structure utilisée dans l'algorithme de Tarjan pour la recherche de composantes fortement connexes
+ * 
+ * Cette structure stocke les informations nécessaires pour chaque sommet
+ */
 typedef struct {
-    int id;          // identifiant du sommet (1..n)
-    int num;         // numéro de découverte (index) : initialement -1
-    int lowlink;     // numéro accessible : initialement -1
-    int onStack;     // booléen (0 ou 1) : est-il dans la pile ?
+    int id;          
+    int num;         
+    int lowlink;     
+    int onStack;     
 } t_tarjan_vertex;
 
 typedef struct {
-    char name[10];  // nom de la composante (C1, C2…)
-    int *vertices;  // tableau dynamique d’identifiants de sommets
-    int count;      // nombre de sommets dans la classe
-    int capacity;   // capacité actuelle du tableau
+    char name[10];  
+    int *vertices;  
+    int count;      
+    int capacity;   
 } t_classe;
 
 
